595851715488121c427e137cb1a6987a895c449e
lessaDev <iagolessaferreira@gmail.com>
2023-03-14 21:28:14 -0300

    Refactor App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1> Adopt Me!</h1>
-      <Pet name="Sansa" animal="Dog" breed="pitbul" />
-      <Pet name="Nevasca" animal="Dog" breed="ViraCoKe" />
       <SearchParams />
     </div>
   );
