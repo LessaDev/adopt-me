98a8fce3ea7f28192a9ac73d130b73f075b76225
LessaDev <iagolessaferreira@gmail.com>
2023-03-24 11:38:25 -0300

    add test's from vite

---
+++ src/__tests__/UseBreedList.test.jsx
@@ -0,0 +1,54 @@
+import { expect, test } from "vitest";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { renderHook, waitFor } from "@testing-library/react";
+import useBreedList from "../useBreedLIst";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      retry: false,
+    },
+  },
+});
+
+test("gives an empty list width no animal provider", async () => {
+  const { result } = renderHook(() => useBreedList(""), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
+
+  const [breedList, status] = result.current;
+  expect(breedList).toHaveLength(0);
+  expect(status).toBe("loading");
+});
+
+test("gives back breeds with an animal", async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+  const { result } = renderHook(() => useBreedList("dog"), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
+
+  await waitFor(() => expect(result.current[1]).toBe("success"));
+
+  const [breedList] = result.current;
+  expect(breedList).toEqual(breeds);
+});
